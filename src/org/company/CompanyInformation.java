10eba3d11dfee6b8955f0f32fa12602ca90fb2f6
Balajipartheban <balajipartheban@gmail.com>
2022-10-14 20:17:06 +0530

    hello

---
+++ src/org/company/CompanyInformation.java
@@ -0,0 +1,27 @@
+package org.company;
+
+public class CompanyInformation {
+	private void companyName() {
+		System.out.println("yamaha");
+	}
+	private void companyId() {
+System.out.println("1111");
+	}
+	private void companyAddress() {
+		System.out.println("kelambakkam");
+
+	}
+	public static void main(String[] args) {
+		CompanyInformation c = new CompanyInformation();
+		c.companyName();
+		c.companyId();
+		c.companyAddress();
+	}
+	
+	
+	
+	
+	
+	
+
+}
